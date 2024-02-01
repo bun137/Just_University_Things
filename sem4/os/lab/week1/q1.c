e177543b6fc86196f8cc038c8c77e8a40de623ea
Shreya Gurram <gurram13775@gmail.com>
2024-02-01 12:18:20 +0530

    adds q1

---
+++ sem4/os/lab/week1/q1.c
@@ -0,0 +1,22 @@
+// Question 1
+// Write a program to create a child process which lists all the executing user
+// processes
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  pid_t child = fork();
+  if (child == -1)
+    return 1;
+  if (child) {
+    int status;
+    waitpid(child, &status, 0);
+    return 0;
+  } else {
+    execl("/usr/bin/ps", "/usr/bin/ps", (char *)NULL);
+    perror("exec failed!");
+  }
+}
