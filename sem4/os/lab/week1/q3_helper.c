a6ed0a5232242f6d69932f6c2457fee46e55f3dc
Shreya Gurram <gurram13775@gmail.com>
2024-02-01 12:19:42 +0530

    adds q3 helper

---
+++ sem4/os/lab/week1/q3_helper.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  int x = atoi(argv[1]);
+  int y = atoi(argv[2]);
+  printf("Product: %d\n", x * y);
+
+  return 0;
+}
