8ed19560db1f4d4ca3af582fe4f0758deeb51bbd
Shreya Gurram <gurram13775@gmail.com>
2024-02-01 12:19:03 +0530

    adds q3

---
+++ sem4/os/lab/week1/q3.c
@@ -0,0 +1,28 @@
+// Write a program which accepts 2 integers x and y. Now use exec to execute
+// another user defined program that prints the product of x and y.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  int x, y;
+  printf("Enter x and y: ");
+  scanf("%d %d", &x, &y);
+  pid_t child = fork();
+  if (child == -1)
+    return 1;
+  if (child) {
+    int status;
+    waitpid(child, &status, 0);
+    return 0;
+  } else {
+    char x_str[10], y_str[10];
+    sprintf(x_str, "%d", x);
+    sprintf(y_str, "%d", y);
+    execl("./q3_helper", "./q3_helper", x_str, y_str, (char *)NULL);
+    perror("exec failed!");
+  }
+}
