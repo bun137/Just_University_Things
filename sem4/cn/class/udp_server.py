847c5115e02ff673210f1914422c3d39b520f3ad
Shreya Gurram <gurram13775@gmail.com>
2024-02-06 13:15:59 +0530

    adds serveri udp

---
+++ sem4/cn/class/udp_server.py
@@ -0,0 +1,12 @@
+import socket
+HOST="127.0.0.1" #loopback address
+PORT=1239 #port number, make sure it is greater than 1024
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    print("UDP server is now listening")
+    while True:
+        data, addr = s.recvfrom(1024)
+        print("Received ", data.decode(), " from ", addr)
+        upper = data.decode().upper()
+        s.sendto(upper.encode(), addr)
+s.close() 
